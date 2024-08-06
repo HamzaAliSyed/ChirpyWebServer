6c08e018f7503689d9306c7684528ab3cfbf61ba
root <root@DESKTOP-CUU4T05.localdomain>
2024-08-07 02:53:40 +0500

    Changed print statement to match with Boot.dev one

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 		Addr:    ":" + port,
 		Handler: mux,
 	}
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 
 }
